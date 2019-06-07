81d3c33af019360e858e173cd7bc84fc9a9511df
XB <ssbandjl@163.com>
2019-06-08 00:11:01 +0800

    init

---
+++ src/views/Info.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>info</div>
+</template>
+
+
+<script>
+export default {
+    name: "Info"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>user</div>
+</template>
+
+
+<script>
+export default {
+    name: "User"
+}
+</script>
+
+<style scoped>
+
+</style>
